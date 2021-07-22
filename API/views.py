b63173fd5a58bf1110749e18038eb0bcfa4765da
BassamHatem <bassamgomaa123@gmail.com>
2021-07-22 16:55:55 +0200

    Multiple models

--- API/views.py
+++ API/views.py
@@ -69,9 +69,8 @@ class DiacritizationView(generics.GenericAPIView):
         serializer = OtherSentenceSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         DNN_input = serializer.validated_data['raw']
-        model = keras.models.load_model('Encoder.sav')
-        '''
-        DNN_output1 = "DIACRITIZED SENTENCE"
+        model1 = keras.models.load_model('LLSTM_weights2.h5')
+        DNN_output1 = predict(DNN_input, model1)
         DNN_output2 = "DIACRITIZED SENTENCE"
         DNN_output3 = "DIACRITIZED SENTENCE"
         if DNN_output2 == DNN_output1:
@@ -88,8 +87,6 @@ class DiacritizationView(generics.GenericAPIView):
             return Response({"Success": "Your request is pending, you will be notified when it is done."}, status=status.HTTP_201_CREATED)
         serializer.save()
         return Response(serializer.validated_data['diacritized'], status=status.HTTP_201_CREATED)
-        '''
-        DNN_output = predict(DNN_input, model)
-        serializer.validated_data['diacritized'] = DNN_output
-        serializer.save()
-        return Response(serializer.validated_data['diacritized'], status=status.HTTP_201_CREATED)
+        # serializer.validated_data['diacritized'] = DNN_output
+        # serializer.save()
+        # return Response(serializer.validated_data['diacritized'], status=status.HTTP_201_CREATED)
